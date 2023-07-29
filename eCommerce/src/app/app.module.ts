80d3832150965ecc871235046f65bb65abfc11d4
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-29 05:00:40 +0300

    home01

--- eCommerce/src/app/app.module.ts
+++ eCommerce/src/app/app.module.ts
@@ -5,11 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { OfferComponent } from './offer/offer.component';
+import { ProductsComponent } from './products/products.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { VendorComponent } from './vendor/vendor.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    FeaturedComponent,
+    CategoriesComponent,
+    OfferComponent,
+    ProductsComponent,
+    SubscribeComponent,
+    VendorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ eCommerce/src/app/vendor/vendor.component.ts
@@ -0,0 +1,51 @@
+import { Component,  AfterViewInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+declare var $: any; // Declare jQuery globally to use it in TypeScript
+@Component({
+  selector: 'app-vendor',
+  templateUrl: './vendor.component.html',
+  styleUrls: ['./vendor.component.scss']
+})
+export class VendorComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    },
+    nav: true
+  };
+
+  ngAfterViewInit() {
+    // Initialize the Owl Carousel after the component is rendered
+    $(".vendor-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    });
+  }
+}
